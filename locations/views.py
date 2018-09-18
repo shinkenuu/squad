36c1c5527a6f9a1c30afde9cccbdb6cfc1fc577d
Shinkenuu <shinkenuu@gmail.com>
2018-09-18 17:35:56 -0300

    changes kwargs parameter names to respect the new pattern (without slug anymore)

--- locations/views.py
+++ locations/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_list_or_404, get_object_or_404
+from django.template.defaultfilters import slugify
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -9,13 +10,15 @@ from .serializers import CitySerializer, StateCitySerializer
 
 class StateCitiesListView(APIView):
     def get(self, request, *args, **kwargs):
-        cities = get_list_or_404(City, state__name_slug=kwargs.get('state_slug'))
+        state_name = slugify(kwargs.get('state_name'))
+        cities = get_list_or_404(City, state__name_slug=state_name)
 
         serializer = StateCitySerializer(cities, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, *args, **kwargs):
-        state = get_object_or_404(State, name_slug=kwargs.get('state_slug'))
+        state_name = slugify(kwargs.get('state_name'))
+        state = get_object_or_404(State, name_slug=state_name)
 
         data = request.data.copy()
         data['state'] = state.id
@@ -30,9 +33,9 @@ class StateCitiesListView(APIView):
 
 class StateCitiesDetailView(APIView):
     def get(self, request, *args, **kwargs):
-        city = get_object_or_404(City,
-                                 name_slug=kwargs.get('city_slug'),
-                                 state__name_slug=kwargs.get('state_slug'))
+        state_name = slugify(kwargs.get('state_name'))
+        city_name = slugify(kwargs.get('city_name'))
+        city = get_object_or_404(City, name_slug=city_name, state__name_slug=state_name)
 
         serializer = StateCitySerializer(city)
         return Response(serializer.data, status=status.HTTP_200_OK)
