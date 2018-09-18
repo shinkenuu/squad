2fe937c617e0f15d63f6a492ef88222e86911fed
Shinkenuu <shinkenuu@gmail.com>
2018-09-18 17:34:33 -0300

    changes slug:state_slug and slug:city_slug to str:state_name and str:city_name

--- locations/urls.py
+++ locations/urls.py
@@ -4,6 +4,6 @@ from .views import StateCitiesDetailView, StateCitiesListView
 
 
 urlpatterns = [
-    path('states/<slug:state_slug>/cities/', StateCitiesListView.as_view(), name='state-cities-list'),
-    path('states/<slug:state_slug>/cities/<slug:city_slug>/', StateCitiesDetailView.as_view(), name='city-detail'),
+    path('states/<str:state_name>/cities/', StateCitiesListView.as_view(), name='state-cities-list'),
+    path('states/<str:state_name>/cities/<str:city_name>/', StateCitiesDetailView.as_view(), name='city-detail'),
 ]
